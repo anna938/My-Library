62b3a05ce0fafa24564b780df01ad78489bfc46a
anna938 <khatami.anhita@gmail.com>
2020-10-25 19:54:15 -0400

    Changing to stateless component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
 import Book from './Book';
-import PropTypes from 'prop-types'
+import propTypes from 'prop-types'
+//import Bookshelf from './Bookshelf';
+const Bookshelf = (props) => {
 
-class Bookshelf extends Component {
-    render() {
-        const { books, title, shelf, updateShelf } = this.props;
 
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{title}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {books
-                            .filter((book) => book.shelf === shelf)
-                            .map((book) => {
-                                return (
-                                    <li key={book.id}>
-                                        <Book key={book.id} book={book} updateShelf={updateShelf} />
-                                    </li>
-                                )
-                            })
-                        }
-                    </ol>
-                </div>
+    const { books, title, shelf, updateShelf } = props;
+
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books
+                        .filter((book) => book.shelf === shelf)
+                        .map((book) => {
+                            return (
+                                <li key={book.id}>
+                                    <Book key={book.id} book={book} updateShelf={updateShelf} currentBooks={books} />
+                                </li>
+                            )
+                        })
+                    }
+                </ol>
             </div>
-        );
-    }
+        </div>
+    );
+
 }
 
 export default Bookshelf;
-Bookshelf.PropTypes = {
-    updateShelf: PropTypes.func.isRequired,
-    books: PropTypes.array.isRequired,
-    title: PropTypes.string.isRequired
+Bookshelf.propTypes = {
+    updateShelf: propTypes.func.isRequired,
+    books: propTypes.array.isRequired,
+    title: propTypes.string.isRequired
 }
