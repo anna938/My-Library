49afc198e415673ce91ab360af1cf33372a49cd7
Anahita Khatami <44297569+anna938@users.noreply.github.com>
2020-11-28 11:10:48 -0500

    Update Bookshelf.js

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Book from './Book';
 import propTypes from 'prop-types'
-//import Bookshelf from './Bookshelf';
+
 const Bookshelf = (props) => {
 
 
